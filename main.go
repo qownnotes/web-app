d9654c5b215b3dd700a3774c80d19468781f47a7
Patrizio Bekerle <patrizio@bekerle.com>
2021-04-16 13:59:34 +0200

    Add chat code to vue.js app and add web server to serve it

--- main.go
+++ main.go
@@ -8,28 +8,20 @@ import (
 
 var addr = flag.String("addr", ":8080", "http service address")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "home.html")
-}
-
 func main() {
 	flag.Parse()
 	hub := newHub()
 	go hub.run()
-	http.HandleFunc("/", serveHome)
+
+	fs := http.FileServer(http.Dir("web/dist"))
+	http.Handle("/", fs)
+
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
+
 	err := http.ListenAndServe(*addr, nil)
+
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
--- web/src/App.vue
+++ web/src/App.vue
@@ -2,12 +2,69 @@
   <v-app>
     <v-main>
       <HelloWorld/>
+
+      <div id="log"></div>
+      <form id="form">
+        <input type="submit" value="Send" />
+        <input type="text" id="msg" size="64" autofocus />
+      </form>
     </v-main>
   </v-app>
 </template>
 
+<style>
+
+</style>
+
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+
+window.onload = function () {
+  var conn;
+  var msg = document.getElementById("msg");
+  var log = document.getElementById("log");
+
+  function appendLog(item) {
+    var doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
+    log.appendChild(item);
+    if (doScroll) {
+      log.scrollTop = log.scrollHeight - log.clientHeight;
+    }
+  }
+
+  document.getElementById("form").onsubmit = function () {
+    if (!conn) {
+      return false;
+    }
+    if (!msg.value) {
+      return false;
+    }
+    conn.send(msg.value);
+    msg.value = "";
+    return false;
+  };
+
+  if (window["WebSocket"]) {
+    conn = new WebSocket("ws://" + document.location.host + "/ws");
+    conn.onclose = function (evt) {
+      var item = document.createElement("div");
+      item.innerHTML = "<b>Connection closed.</b>";
+      appendLog(item);
+    };
+    conn.onmessage = function (evt) {
+      var messages = evt.data.split('\n');
+      for (var i = 0; i < messages.length; i++) {
+        var item = document.createElement("div");
+        item.innerText = messages[i];
+        appendLog(item);
+      }
+    };
+  } else {
+    var item = document.createElement("div");
+    item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
+    appendLog(item);
+  }
+};
 
 export default {
   name: 'App',
