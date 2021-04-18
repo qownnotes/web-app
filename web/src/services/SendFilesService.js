06fdd226d6b0ade134af0920ee8d8101cda59f4d
Patrizio Bekerle <patrizio@bekerle.com>
2021-04-18 18:32:43 +0200

    Enable file sending again

--- web/src/services/SendFilesService.js
+++ web/src/services/SendFilesService.js
@@ -15,38 +15,30 @@ class SendFilesService {
             return;
         }
 
-        console.log("Send data...");
-        console.log(file);
-        window.ws.send("My data");
+        // console.log("Send data...");
+        // console.log(file);
+        // window.ws.send("My data");
 
-    //     console.log("Send file...");
-    //     console.log(file);
-    //     let reader = new FileReader();
-    //
-    //     reader.onerror = () => {
-    //         console.error(reader.error);
-    //     };
-    //
-    //     reader.onload = (e) => {
-    //         console.log(e.target.result);
-    //         const data = {
-    //             "room": "first-room",
-    //             "msg": "my message",
-    //             "token": "123",
-    //             "command": "insertFile",
-    //             // "file": e.target.result
-    //         }
-    //
-    //         console.log(data);
-    //         // window.ws.send(data);
-    //         window.ws.send(JSON.stringify(data));
-    //
-    //         // window.ws.close();
-    //         // TODO: Implement an acknowledge answer from the server for every client message
-    //     }
-    //
-    //     reader.readAsDataURL(file);
-    //     // window.ws.onopen = () => reader.readAsDataURL(file);
+        console.log("Send file", file);
+        let reader = new FileReader();
+
+        reader.onerror = () => {
+            console.error("Reader error", reader.error);
+        };
+
+        reader.onload = (e) => {
+            console.log(e.target.result);
+            const data = {
+                "command": "insertFile",
+                "file": e.target.result
+            }
+
+            window.ws.send(JSON.stringify(data));
+
+            // TODO: Implement an acknowledge answer from the server for every client message
+        }
+
+        reader.readAsDataURL(file);
     }
 }
 
