b743b95633aa9c839be81d66827a6dd46a2e2c48
Patrizio Bekerle <patrizio@bekerle.com>
2021-04-21 08:43:56 +0200

    Update app name and title

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+          args[0].title = "QOwnNotes Web App";
+          return args;
+        })
+  },
   transpileDependencies: [
     'vuetify'
   ],
