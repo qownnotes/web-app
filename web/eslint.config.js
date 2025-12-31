1ded94350f2d3591715e9ae5a75d40108926174c
Patrizio Bekerle <patrizio@bekerle.com>
2025-12-31 08:29:46 +0100

    eslint: add config
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

---
+++ web/eslint.config.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    files: ["**/*.js", "**/*.vue"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      globals: {
+        // Node.js globals
+        process: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        module: "readonly",
+        require: "readonly",
+        exports: "readonly",
+        // Browser globals
+        window: "readonly",
+        document: "readonly",
+        navigator: "readonly",
+        console: "readonly",
+        localStorage: "readonly",
+        sessionStorage: "readonly",
+        fetch: "readonly",
+        alert: "readonly",
+        confirm: "readonly",
+        // Vue/Webpack globals
+        Vue: "readonly",
+      },
+    },
+    rules: {
+      "no-console": "off",
+      "no-debugger": "warn",
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "build/**",
+      "public/**",
+      "*.config.js",
+      "babel.config.js",
+      "vue.config.js",
+    ],
+  },
+];
